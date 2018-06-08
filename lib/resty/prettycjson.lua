47c459549fba6ed8debafc767f1df3b3e8c7c9db
LunaCI <lunaci@luadist.org>
2018-06-08 01:34:35 +0000

    Update to version 1.3-1

--- lib/resty/prettycjson.lua
+++ lib/resty/prettycjson.lua
@@ -1,45 +1,38 @@
-local json = require "cjson.safe".encode
-local concat = table.concat
+local enc = require "cjson.safe".encode
+local cat = table.concat
 local sub = string.sub
 local rep = string.rep
-return function(tbl, lf, id, ac)
-    local s, e = json(tbl)
+return function(dt, lf, id, ac)
+    local s, e = enc(dt)
     if not s then return s, e end
     lf, id, ac = lf or "\n", id or "\t", ac or " "
-    local i, j, k, r, p, q  = 1, 0, 0, {}, nil, nil
+    local i, j, k, n, r, p, q  = 1, 0, 0, #s, {}, nil, nil
     local al = sub(ac, -1) == "\n"
-    local n = #s
     for x = 1, n do
         local c = sub(s, x, x)
         if not q and (c == "{" or c == "[") then
-            r[i] = p == ":" and concat{ c, lf } or concat{ rep(id, j), c, lf }
+            r[i] = p == ":" and cat{ c, lf } or cat{ rep(id, j), c, lf }
             j = j + 1
         elseif not q and (c == "}" or c == "]") then
             j = j - 1
             if p == "{" or p == "[" then
                 i = i - 1
-                r[i] = concat{ rep(id, j), p, c }
+                r[i] = cat{ rep(id, j), p, c }
             else
-                r[i] = concat{ lf, rep(id, j), c }
+                r[i] = cat{ lf, rep(id, j), c }
             end
         elseif not q and c == "," then
-            r[i] = concat{ c, lf }
+            r[i] = cat{ c, lf }
             k = -1
         elseif not q and c == ":" then
-            r[i] = concat{ c, ac }
+            r[i] = cat{ c, ac }
             if al then
                 i = i + 1
                 r[i] = rep(id, j)
             end
         else
-            if c == '"' or c == "'" then
-                if p ~= "\\" then
-                    if q == c then
-                        q = nil
-                    elseif not q then
-                        q = c
-                    end
-                end
+            if c == '"' and p ~= "\\" then
+                q = not q and true or nil
             end
             if j ~= k then
                 r[i] = rep(id, j)
@@ -49,5 +42,5 @@ return function(tbl, lf, id, ac)
         end
         p, i = c, i + 1
     end
-    return concat(r)
+    return cat(r)
 end
